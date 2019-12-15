74291de280e08b5a9939896fe0786282c343c7ca
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-12-15 16:37:41 +0100

    Another try D.O.M.

---
+++ app.js
@@ -0,0 +1,17 @@
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const input = document.querySelector('input');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button');
+
+toggleList.addEventListener('click', () => {
+    if (listDiv.style.display == 'none' ) {
+        listDiv.style.display = 'block';
+    } else {
+    listDiv.style.display = 'none';
+    }
+});
+
+button.addEventListener('click', () => {
+    p.innerHTML = input.value + ':';
+});
